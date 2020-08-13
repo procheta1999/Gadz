fd25380da26c533924bd3d857177622e3491dfbb
procheta1999 <bhattacharyya.procheta1999@gmail.com>
2020-08-13 22:56:56 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <hr></hr>
+      <p class="line-1 anim-typewriter">Gadz</p>
+        <center><h2 style={{fontSize:25}}>Gadget Reviews at Your Fingertips</h2></center>
+<img src="https://media.giphy.com/media/3oKIPtaS9TtrmIzXRm/giphy.gif"></img>
+      </header>
+    </div>
+  );
+}
+
+export default App;
